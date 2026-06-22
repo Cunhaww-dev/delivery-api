7e9549e8e6541be75aa5fb186dc6323b1f0ca79e
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-06-22 17:42:52 -0300

    feat: add user data validate with zod and use bcrypt hash for password

--- src/controller/users-controller.ts
+++ src/controller/users-controller.ts
@@ -1,7 +1,19 @@
+import { hash } from 'bcrypt';
 import { Request, Response } from 'express';
+import z from 'zod';
 
 export class UserController {
-  create(req: Request, res: Response) {
-    return res.json({ message: 'ok' });
+  async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      name: z.string().min(2),
+      email: z.email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(req.body);
+
+    const hashedPass = await hash(password, 8);
+
+    return res.json({ hashedPass });
   }
 }
