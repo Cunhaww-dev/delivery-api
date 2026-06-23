3636a29087b5012d7daf25cda39c32cd82eadafb
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-06-23 10:41:50 -0300

    feat: user creation validation and password hashing

--- src/controller/users-controller.ts
+++ src/controller/users-controller.ts
@@ -1,3 +1,5 @@
+import { prisma } from '@/database/prisma';
+import { AppError } from '@/utils/AppError';
 import { hash } from 'bcrypt';
 import { Request, Response } from 'express';
 import z from 'zod';
@@ -12,8 +14,26 @@ export class UserController {
 
     const { name, email, password } = bodySchema.parse(req.body);
 
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (userWithSameEmail) {
+      throw new AppError('Email already exists', 409);
+    }
+
     const hashedPass = await hash(password, 8);
 
-    return res.json({ hashedPass });
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPass,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return res.status(201).json(userWithoutPassword);
   }
 }
