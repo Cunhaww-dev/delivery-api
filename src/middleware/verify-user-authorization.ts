6141fb6b6539fe75ff5d25b4abf25de2fe054cf7
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-06-26 17:21:26 -0300

    fix: correct typo in verifyUserAuthorization function name

--- src/middleware/verify-user-authorization.ts
+++ src/middleware/verify-user-authorization.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { AppError } from '@/utils/AppError';
 
-export function verifyUserAUthorization(role: String[]) {
+export function verifyUserAuthorization(role: String[]) {
   return (req: Request, res: Response, next: NextFunction) => {
     if (!req.user) {
       throw new AppError('Unauthorized', 401);
